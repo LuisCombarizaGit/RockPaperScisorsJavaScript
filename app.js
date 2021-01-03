1487410b816bb7ede6e3d61bb6fae302fd700fd4
Luis Combariza <luis_combariza@outlook.com>
2021-01-02 20:37:30 -0700

    Updated .js file

--- app.js
+++ app.js
@@ -28,10 +28,31 @@ const game = () => {
     const computerHand = document.querySelector(".computer-hand");
 
     // Computer choice options by using random()
-    const computerOpotions = ["rock", "paper", "scissors"];
-    const computerNumber = Math.floor(Math.random() * 3);
-    console.log(computerNumber);
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option=>{
+        option.addEventListener("click", function(){
+             const computerNumber = Math.floor(Math.random() * 3);
+             const computerChoice = computerOptions[computerNumber];
+            
+
+        });
+    });
+
   };
+
+  const compareHands = (playerChoice, computerChoice)=> {
+      const winner = document.querySelector(".winner");
+      // Check for a tie
+      if(playerChoice === computerChoice){
+          winner.textContent = "It is a tie";
+          return;
+      }
+      // Check for Rock
+      if(playerChoice === 'Rock')
+
+  }
+
 
   // Invoke inner functions
   startGame();
